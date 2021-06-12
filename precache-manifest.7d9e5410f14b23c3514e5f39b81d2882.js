462e701e9546a221f6b30145ccea1164163ab274
Bar Horing Amir <bar.apple@gmail.com>
2021-06-12 17:47:08 +0300

    Updates

---
+++ precache-manifest.7d9e5410f14b23c3514e5f39b81d2882.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342dcc725d49bca8ceb5caf0ba1d9da2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22b62152ce3d92f45d92",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "27a31b4f7a6cf7cabd19",
+    "url": "/static/css/main.6012c55a.chunk.css"
+  },
+  {
+    "revision": "22b62152ce3d92f45d92",
+    "url": "/static/js/2.abae1c8a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.abae1c8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a31b4f7a6cf7cabd19",
+    "url": "/static/js/main.9f7e1ead.chunk.js"
+  },
+  {
+    "revision": "4da1af9a618e66224825",
+    "url": "/static/js/runtime-main.2da419cc.js"
+  }
+]);
